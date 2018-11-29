3f607cb552096364a66a746ff96893df515f644d
Glitch (pleasant-router) <none>
2018-11-29 10:15:39 +0000

    🎪🏫 Checkpoint ./server.js:2177980/3694 ./User.js:2177980/221 ./views/index.html:2177980/3401

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
+const User = require('./User');
+
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -21,7 +23,8 @@ app.get('/', (req, res) => {
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+  //return next({status: 404, message: 'not found'})
+  return next();
 })
 
 // Error Handling middleware
@@ -42,6 +45,65 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+
+
+//================================================================================
+app.get('/api/exercise/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return res.status(500).send({ message: err.message });
+    }
+    
+    return res.send(users);
+  });
+});
+
+app.post('/api/exercise/new-user', (req, res) => {
+  const user = new User({
+    username: req.body.username
+  });
+  
+  user.save((err, doc) => {
+    if (err) {
+      return res.status(500).send({ message: err.message });
+    }
+    
+    return res.send(doc);
+  });
+});
+
+app.post('/api/exercise/add', (req, res) => {
+  const exercise = {
+    description: req.body.description,
+    duration: req.body.duration,
+    date: req.body.date ? req.body.date : new Date()
+  }
+  
+  User.findByIdAndUpdate(req.body.userId, { 
+    "$push": { "exercise": exercise } 
+  }, { new: true }, (err, doc) => {
+    console.log(doc)
+    if (err) {
+      return res.status(500).send({message: err.message});
+    }
+  
+    res.send(doc);
+  });
+});
+
+app.get('/api/exercise/log', (req, res) => {
+
+  User.findById(req.query.userId, (err, doc) => {
+    if (err) {
+      return res.status(500).send({message: err.message});
+    }
+    
+    res.send(doc);
+  });
+  
+
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
